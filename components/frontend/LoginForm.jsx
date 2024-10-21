05cb86693ff902fb25bada1f800e283f6bd46cef
Rayner Ray <f13ray@gmail.com>
2024-10-21 13:22:52 +0200

    new login

--- components/frontend/LoginForm.jsx
+++ components/frontend/LoginForm.jsx
@@ -134,7 +134,7 @@ export default function LoginForm() {
       </div>
 
       <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-        Already have an account?{" "}
+        Don't have an account?{" "}
         <Link
           href="/register"
           className="font-medium text-green-600 hover:underline dark:text-green-500"
